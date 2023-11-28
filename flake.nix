46eefe76cefb1d4081a867d149cff038809bc1a0
Albin Vass <albin.vass@gmail.com>
2023-11-28 20:24:10 +0100

    Fix allowUnfree

--- flake.nix
+++ flake.nix
@@ -13,7 +13,10 @@
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in {
       homeConfigurations."vass" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
